5708dd910051d80b6d2ff9d029013739ad8004ef
Jesus Lopez <jesus@ascend.travel>
2025-05-08 16:26:36 -0300

    feat: Integrate Intercom Messenger SDK into AddTripsView component
    This commit adds the Intercom Messenger SDK to the AddTripsView component, enabling user identification with Intercom for enhanced user engagement. The SDK is initialized with user details such as user ID, name, and email.

--- src/app/(hotel-rp-pages)/add-trip/_components/AddTripsView.tsx
+++ src/app/(hotel-rp-pages)/add-trip/_components/AddTripsView.tsx
@@ -10,6 +10,7 @@ import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/
 import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 import { useUser } from '@/contexts/UserContext';
+import Intercom from '@intercom/messenger-js-sdk';
 
 import { CopyIcon, Plus } from 'lucide-react';
 import { toast } from 'sonner';
@@ -18,6 +19,13 @@ export default function AddTripsView() {
     const { user, reloadUser } = useUser();
     const [isAccountsExpanded, setIsAccountsExpanded] = useState(true);
     const router = useRouter();
+
+    Intercom({
+        app_id: 'otkzzyb8',
+        user_id: user?.id ?? '',
+        name: user?.first_name ?? '',
+        email: user?.main_email ?? ''
+    });
 
     useEffect(() => {
         reloadUser();
