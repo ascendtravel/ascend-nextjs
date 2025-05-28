ed2ce0e465ab8a164304210a06d4f44b44af01b4
Nicolas Arias <nicolas.pulido@ascend.travel>
2025-05-28 11:46:10 -0500

    Update marquee data and gmail check image

--- src/app/(top-funnel)/welcome/_components/OnboardingGmailCheckCta.tsx
+++ src/app/(top-funnel)/welcome/_components/OnboardingGmailCheckCta.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image';
 
 export default function OnboardingGmailCheckCta() {
     return (
-        <div className='flex h-[200px] w-full items-center justify-center'>
+        <div className='flex h-[200px] w-full items-center justify-center px-2'>
             <div className='relative flex h-full w-full items-center justify-center'>
-                <div className='absolute inset-0 flex w-full items-center justify-center'>
+                <div className='absolute inset-0 flex w-full items-center justify-center px-3'>
                     {/* Optimize for iphone  */}
                     <Image
                         src='/onboarding-assets/gmail-check-user-cta.png'
---
+++ src/app/(top-funnel)/welcome/_components/savedData.ts
@@ -0,0 +1,831 @@
+export default [
+  {
+      "amount": 130.6,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "LHR",
+      "departure_airport": "BOS"
+  },
+  {
+      "amount": 1249.86,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Tampa Marriott Water Street"
+  },
+  {
+      "amount": 339.11,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Marriott Stanton South Beach"
+  },
+  {
+      "amount": 150.2,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "AMS",
+      "departure_airport": "TPA"
+  },
+  {
+      "amount": 419.6,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Warwick Denver"
+  },
+  {
+      "amount": 3004.3,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Casa Mexicana Cozumel"
+  },
+  {
+      "amount": 359.63,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Staybridge Suites Myrtle Beach - West"
+  },
+  {
+      "amount": 543.06,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "CUN",
+      "departure_airport": "BWI"
+  },
+  {
+      "amount": 241.92,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "MCO",
+      "departure_airport": "HOU"
+  },
+  {
+      "amount": 110.18,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "LAX",
+      "departure_airport": "EWR"
+  },
+  {
+      "amount": 397.05,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Trump National Doral"
+  },
+  {
+      "amount": 420,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "DEN",
+      "departure_airport": "BWI"
+  },
+  {
+      "amount": 159.48,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "LGA",
+      "departure_airport": "ORD"
+  },
+  {
+      "amount": 386.18,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "JW Marriott Tampa Water Street"
+  },
+  {
+      "amount": 104.97,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "PDX",
+      "departure_airport": "LAX"
+  },
+  {
+      "amount": 1237.32,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Tampa Marriott Water Street"
+  },
+  {
+      "amount": 339.11,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Marriott Stanton South Beach"
+  },
+  {
+      "amount": 112.96,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "DEN",
+      "departure_airport": "STL"
+  },
+  {
+      "amount": 261,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Warwick Denver"
+  },
+  {
+      "amount": 323.81,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Hyatt Regency Austin"
+  },
+  {
+      "amount": 110.22,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "LAX",
+      "departure_airport": "EWR"
+  },
+  {
+      "amount": 241.92,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "MCO",
+      "departure_airport": "HOU"
+  },
+  {
+      "amount": 110.18,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "LAX",
+      "departure_airport": "EWR"
+  },
+  {
+      "amount": 159.48,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "LGA",
+      "departure_airport": "ORD"
+  },
+  {
+      "amount": 144.97,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "PDX",
+      "departure_airport": "LAX"
+  },
+  {
+      "amount": 1231.83,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Tampa Marriott Water Street"
+  },
+  {
+      "amount": 1249.86,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Tampa Marriott Water Street"
+  },
+  {
+      "amount": 328.74,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "JW Marriott Tampa Water Street"
+  },
+  {
+      "amount": 339.11,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Marriott Stanton South Beach"
+  },
+  {
+      "amount": 340,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Staybridge Suites Myrtle Beach - West"
+  },
+  {
+      "amount": 796.21,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Hyatt Regency Austin"
+  },
+  {
+      "amount": 519.96,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "SEA",
+      "departure_airport": "SLC"
+  },
+  {
+      "amount": 141.92,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "MCO",
+      "departure_airport": "HOU"
+  },
+  {
+      "amount": 100.81,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "AMS",
+      "departure_airport": "TPA"
+  },
+  {
+      "amount": 138.51,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "GVA",
+      "departure_airport": "SEA"
+  },
+  {
+      "amount": 138.51,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "GVA",
+      "departure_airport": "SEA"
+  },
+  {
+      "amount": 139.96,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "BNA",
+      "departure_airport": "PHL"
+  },
+  {
+      "amount": 1199.46,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Tampa Marriott Water Street"
+  },
+  {
+      "amount": 286.96,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "JW Marriott Tampa Water Street"
+  },
+  {
+      "amount": 1249.86,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Tampa Marriott Water Street"
+  },
+  {
+      "amount": 380.87,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Marriott Stanton South Beach"
+  },
+  {
+      "amount": 104.36,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "SEA",
+      "departure_airport": "PSP"
+  },
+  {
+      "amount": 126.96,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "DEN",
+      "departure_airport": "STL"
+  },
+  {
+      "amount": 406.65,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Staybridge Suites Myrtle Beach - West"
+  },
+  {
+      "amount": 181.92,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "MCO",
+      "departure_airport": "HOU"
+  },
+  {
+      "amount": 159.36,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "ALB",
+      "departure_airport": "IAD"
+  },
+  {
+      "amount": 279.4,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "TPE",
+      "departure_airport": "ORD"
+  },
+  {
+      "amount": 159.48,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "LGA",
+      "departure_airport": "ORD"
+  },
+  {
+      "amount": 139.96,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "BNA",
+      "departure_airport": "PHL"
+  },
+  {
+      "amount": 339.11,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Marriott Stanton South Beach"
+  },
+  {
+      "amount": 408.33,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Staybridge Suites Myrtle Beach - West"
+  },
+  {
+      "amount": 181.92,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "MCO",
+      "departure_airport": "HOU"
+  },
+  {
+      "amount": 279.4,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "TPE",
+      "departure_airport": "ORD"
+  },
+  {
+      "amount": 159.48,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "LGA",
+      "departure_airport": "ORD"
+  },
+  {
+      "amount": 356.86,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Marriott Stanton South Beach"
+  },
+  {
+      "amount": 194.96,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "AEX",
+      "departure_airport": "SJC"
+  },
+  {
+      "amount": 117.02,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "GUA",
+      "departure_airport": "OKC"
+  },
+  {
+      "amount": 106,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "ATH",
+      "departure_airport": "EWR"
+  },
+  {
+      "amount": 187.81,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "AMS",
+      "departure_airport": "TPA"
+  },
+  {
+      "amount": 418.79,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Staybridge Suites Myrtle Beach - West"
+  },
+  {
+      "amount": 181.92,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "MCO",
+      "departure_airport": "HOU"
+  },
+  {
+      "amount": 159.48,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "LGA",
+      "departure_airport": "ORD"
+  },
+  {
+      "amount": 355.81,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Marriott Stanton South Beach"
+  },
+  {
+      "amount": 117.96,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "LGA",
+      "departure_airport": "PIT"
+  },
+  {
+      "amount": 117.96,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "LGA",
+      "departure_airport": "PIT"
+  },
+  {
+      "amount": 117.96,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "LGA",
+      "departure_airport": "PIT"
+  },
+  {
+      "amount": 199.81,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "AMS",
+      "departure_airport": "TPA"
+  },
+  {
+      "amount": 391.6,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Staybridge Suites Myrtle Beach - West"
+  },
+  {
+      "amount": 203.92,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "MCO",
+      "departure_airport": "HOU"
+  },
+  {
+      "amount": 129.48,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "LGA",
+      "departure_airport": "ORD"
+  },
+  {
+      "amount": 396.82,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Staybridge Suites Myrtle Beach - West"
+  },
+  {
+      "amount": 121.99,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "MNL",
+      "departure_airport": "GUM"
+  },
+  {
+      "amount": 199.81,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "AMS",
+      "departure_airport": "TPA"
+  },
+  {
+      "amount": 129.48,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "LGA",
+      "departure_airport": "ORD"
+  },
+  {
+      "amount": 101.39,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "POP",
+      "departure_airport": "ATL"
+  },
+  {
+      "amount": 123.7,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "HNL",
+      "departure_airport": "DAL"
+  },
+  {
+      "amount": 396.82,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Staybridge Suites Myrtle Beach - West"
+  },
+  {
+      "amount": 191.97,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "DCA",
+      "departure_airport": "HOU"
+  },
+  {
+      "amount": 134.6,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "LGA",
+      "departure_airport": "BNA"
+  },
+  {
+      "amount": 399.41,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "AMS",
+      "departure_airport": "LAX"
+  },
+  {
+      "amount": 315.91,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "PMO",
+      "departure_airport": "EWR"
+  },
+  {
+      "amount": 165.48,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "LGA",
+      "departure_airport": "ORD"
+  },
+  {
+      "amount": 288.04,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "MCO",
+      "departure_airport": "DEN"
+  },
+  {
+      "amount": 121.96,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "MCO",
+      "departure_airport": "BNA"
+  },
+  {
+      "amount": 101.39,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "POP",
+      "departure_airport": "ATL"
+  },
+  {
+      "amount": 149.66,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "MCI",
+      "departure_airport": "FLL"
+  },
+  {
+      "amount": 147.38,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "LGA",
+      "departure_airport": "MTJ"
+  },
+  {
+      "amount": 396.82,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Staybridge Suites Myrtle Beach - West"
+  },
+  {
+      "amount": 119.57,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "HOU",
+      "departure_airport": "DCA"
+  },
+  {
+      "amount": 119.91,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "SFO",
+      "departure_airport": "PIT"
+  },
+  {
+      "amount": 152.96,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "SEA",
+      "departure_airport": "SLC"
+  },
+  {
+      "amount": 129.48,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "LGA",
+      "departure_airport": "ORD"
+  },
+  {
+      "amount": 204.04,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "MCO",
+      "departure_airport": "DEN"
+  },
+  {
+      "amount": 527.68,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "PVR",
+      "departure_airport": "YXJ"
+  },
+  {
+      "amount": 304.11,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "SIN",
+      "departure_airport": "SLC"
+  },
+  {
+      "amount": 230.93,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Shangri-La Mactan, Cebu"
+  },
+  {
+      "amount": 120,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "SFO",
+      "departure_airport": "PIT"
+  },
+  {
+      "amount": 129.48,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "LGA",
+      "departure_airport": "ORD"
+  },
+  {
+      "amount": 239.88,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "MCO",
+      "departure_airport": "DEN"
+  },
+  {
+      "amount": 119.37,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "SAN",
+      "departure_airport": "CAE"
+  },
+  {
+      "amount": 201.21,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "DSS",
+      "departure_airport": "JFK"
+  },
+  {
+      "amount": 361,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "ICN",
+      "departure_airport": "SFO"
+  },
+  {
+      "amount": 239.29,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Shangri-La Mactan, Cebu"
+  },
+  {
+      "amount": 104,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "BWI",
+      "departure_airport": "LAX"
+  },
+  {
+      "amount": 119.91,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "SFO",
+      "departure_airport": "PIT"
+  },
+  {
+      "amount": 388.63,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Harrah's Las Vegas"
+  },
+  {
+      "amount": 130,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "LGA",
+      "departure_airport": "ORD"
+  },
+  {
+      "amount": 120,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "SAN",
+      "departure_airport": "CAE"
+  },
+  {
+      "amount": 240.04,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "MCO",
+      "departure_airport": "DEN"
+  },
+  {
+      "amount": 248.69,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Shangri-La Mactan, Cebu"
+  },
+  {
+      "amount": 151.96,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "ATL",
+      "departure_airport": "DFW"
+  },
+  {
+      "amount": 185.9,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "YYZ",
+      "departure_airport": "ICN"
+  },
+  {
+      "amount": 140.4,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "HNL",
+      "departure_airport": "GTF"
+  },
+  {
+      "amount": 140.4,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "HNL",
+      "departure_airport": "GTF"
+  },
+  {
+      "amount": 119.91,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "SFO",
+      "departure_airport": "PIT"
+  },
+  {
+      "amount": 394.9,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Harrah's Las Vegas"
+  },
+  {
+      "amount": 269.58,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Shangri-La Mactan, Cebu"
+  },
+  {
+      "amount": 327.6,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "STL",
+      "departure_airport": "SEA"
+  },
+  {
+      "amount": 169.9,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "YYZ",
+      "departure_airport": "ICN"
+  },
+  {
+      "amount": 209.91,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "SFO",
+      "departure_airport": "PIT"
+  },
+  {
+      "amount": 149.91,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "FCO",
+      "departure_airport": "JFK"
+  },
+  {
+      "amount": 384.46,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Harrah's Las Vegas"
+  },
+  {
+      "amount": 105.02,
+      "currency": "USD",
+      "booking_type": "flight",
+      "arrival_airport": "GUA",
+      "departure_airport": "OKC"
+  },
+  {
+      "amount": 261.22,
+      "currency": "USD",
+      "booking_type": "hotel",
+      "hotel_name": "Shangri-La Mactan, Cebu"
+  }
+]
